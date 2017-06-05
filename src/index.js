ffda81397f05aafaf82afe0d4f65dcbe0e10f42b
Tinus Wagner <tinus@3months.com>
2017-06-05 18:12:30 +1200

    set up my reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers/index.js'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let store = createStore(todoApp)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
